65ed73dfc3d83d2bf4d654a25b2e446d83993b00
xrip <xrip@xrip.ru>
2020-05-01 11:15:48 +0300

    Fixes orFail type definitions issue, based on https://github.com/DefinitelyTyped/DefinitelyTyped/issues/34205#issuecomment-621976826

--- index.d.ts
+++ index.d.ts
@@ -22,6 +22,8 @@ declare module 'cachegoose' {
 declare module 'mongoose' {
 	// eslint-disable-next-line @typescript-eslint/class-name-casing
 	interface DocumentQuery<T, DocType extends Document, QueryHelpers = {}> {
+		// not cachegoose related fix, but usefull. thanks to https://github.com/DefinitelyTyped/DefinitelyTyped/issues/34205#issuecomment-621976826
+		orFail(err?: Error | (() => Error)): DocumentQuery<NonNullable<T>, DocType, QueryHelpers>;
 		cache(ttl: number = 60, customKey: string = ''): this
 		cache(customKey: string = ''): this
 		cache(ttl: number = 60): this
