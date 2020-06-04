c5827caf08bcb5d195a355ea2f3559f812647059
Akhil-bridge <akhil.h@bridge-global.com>
2020-06-04 19:18:57 +0530

    fix(ignorig module declaration issues):

--- index.d.ts
+++ index.d.ts
@@ -15,15 +15,21 @@ declare module 'cachegoose' {
 
 		function clearCache(customKey: any, cb: any): void;
 	}
-
+	// @ts-ignore
 	export = cachegoose;
 }
 
+// @ts-ignore
 declare module 'mongoose' {
 	// eslint-disable-next-line @typescript-eslint/class-name-casing
 	interface DocumentQuery<T, DocType extends Document, QueryHelpers = {}> {
+		// @ts-ignore
 		cache(ttl: number = 60, customKey: string = ''): this
+
+		// @ts-ignore
 		cache(customKey: string = ''): this
+
+		// @ts-ignore
 		cache(ttl: number = 60): this
 	}
 }
